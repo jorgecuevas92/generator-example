ccaa2f82608bcd6aaae0b7c671f2b59ff0d1e0ac
Jorge Cuevas <jorge.cuevas.92@gmail.com>
2020-05-14 23:57:03 -0500

    Added simple prompt and a template that uses the title set

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,6 +6,36 @@ module.exports = class extends Generator {
     super(args, opts);
     this.option('babel');
   }
+
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?"
+      },
+      {
+        type    : 'input',
+        name    : 'title',
+        message : 'Your project title',
+      }
+    ]);
+  }
+
+  writing() {
+    this.log("cool feature log and stuff", this.answers.cool); // user answer `cool` used
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'),
+      { title: this.answers.title }
+    );
+  }
   
   method1() {
     this.log('method 1 just ran');
